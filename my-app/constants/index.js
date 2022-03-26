4dd76984fa79827cd5fb4f3ee4e69c9ef330fb97
st0w <st0w@st0w.com>
2022-03-26 07:45:35 -0400

    Front-end

---
+++ my-app/constants/index.js
@@ -0,0 +1,8 @@
+import CryptoDevToken from "../../hardhat-tutorial/contracts/artifacts/CryptoDevToken.json";
+import CryptoDevs from "../../hardhat-tutorial/contracts/artifacts/CryptoDevs.json";
+
+export const NFT_CONTRACT_ABI = CryptoDevs.abi;
+export const NFT_CONTRACT_ADDRESS = "0xE6efb02906e4C13636235f2Ff83706859E85dc32"; // Rinkeby
+
+export const TOKEN_CONTRACT_ABI = CryptoDevToken.abi;
+export const TOKEN_CONTRACT_ADDRESS = "0x5eAd3E65F6E81cE6C7Fbc59Ac6Fd6bbF34AF0219"; // Rinkeby
